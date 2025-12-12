b57a09b9fd5b76be4c993e23e44d866ab07a8ffe
Tryndalong <dragonljl599@gmail.com>
2025-12-12 22:27:21 +0800

    04 add API

---
+++ app/api/users/route.ts
@@ -0,0 +1,32 @@
+import { db } from "@/config/db";
+import { usersTable } from "@/config/schema";
+import { currentUser } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import { eq } from "drizzle-orm";
+
+export async function POST(req: NextRequest) {
+
+    const user = await currentUser();
+
+    // if User already exists?
+    const userResult = await db.select().from(usersTable)
+        //@ts-ignore
+        .where(eq(usersTable.email, user?.primaryEmailAddress?.emailAddress));
+
+    // if Not then insert new user
+    if (userResult?.length == 0) {
+        const data = {
+            name: user?.fullName ?? 'NA',
+            email: user?.primaryEmailAddress?.emailAddress ?? '',
+            credits: 2,
+        }
+
+        const result = await db.insert(usersTable).values({
+            ...data
+        })
+        return NextResponse.json({ user });
+    }
+
+
+    return NextResponse.json({ user: userResult[0] });
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Provider from "./provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,7 +22,10 @@ export default function RootLayout({
         <body
           className={outfit.className}
         >
-          {children}
+          <Provider>
+            {children}
+          </Provider>
+
         </body>
       </html>
     </ClerkProvider>
