4aacd58d633e2ca6c4105d6287041701725aae20
Tryndalong <dragonljl599@gmail.com>
2025-12-13 20:24:34 +0800

    05 React context Hooks

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: NextRequest) {
         const result = await db.insert(usersTable).values({
             ...data
         })
-        return NextResponse.json({ user });
+        return NextResponse.json({ user: data });
     }
 
 
--- app/provider.tsx
+++ app/provider.tsx
@@ -2,13 +2,16 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
 import { useUser } from '@clerk/nextjs'
+import { User } from 'lucide-react';
+import { UserDetailContext } from '@/context/UserDetailContext';
+import { set } from 'date-fns';
 
 function Provider({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-
+    const [userDetail, setUserDetail] = React.useState<any>(null);
     const { user } = useUser();
     useEffect(() => {
         user && CreateNewUser();
@@ -18,10 +21,13 @@ function Provider({
         const result = await axios.post('/api/users', {
         })
         console.log(result.data);
+        setUserDetail(result.data?.user);
     }
 
     return (
-        <div>{children}</div>
+        <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+            <div>{children}</div>
+        </UserDetailContext.Provider>
     )
 }
 
